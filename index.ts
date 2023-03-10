94a7e2e1e27d9d0370c0f910ceec338690d38df6
diego tonini <diego.tonini93@gmail.com>
2023-03-10 23:22:53 +0100

    init project with simple typescript fastify server

---
+++ index.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+
+const server = fastify({
+    logger: true
+});
+
+server.get("/ping", async (request, reply) => {
+  return "pooong\n";
+});
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
