258c52e2f56cb5f914b76c30d8a7593379a892d8
diego tonini <diego.tonini93@gmail.com>
2023-03-23 21:10:44 +0100

    feat: add simple watcher

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import { loadConfig } from "./utils";
 import { handler as homeHandler } from "./routes/home";
 import { handler as emailLangVariants } from "./routes/list-languages";
 import { handler as emailHandler } from "./routes/email";
+import watch from "node-watch";
+import path from "path";
 
 loadConfig().then((config) => {
   const { inputFolder } = config;
@@ -33,6 +35,31 @@ loadConfig().then((config) => {
   //   (request.params as any).config = 3;
   //   done();
   // });
+
+  const watcher = watch(
+    path.join(inputFolder),
+    { recursive: true, filter: /\.json$/ },
+    function (evt, name) {
+      // example input/buy/it/variables.json
+      if (evt === "update") {
+        const parts = name.split("/");
+        const emailName = parts[1];
+        const locale = parts[2];
+        if (name.endsWith("variables.json")) {
+          // chnged variables
+          console.log("need to refresh", emailName, locale);
+        }
+        if (name.endsWith("common.json")) {
+          // changed common
+        }
+      }
+    }
+  );
+
+  server.addHook("onClose", (_, done) => {
+    watcher.close();
+    done();
+  });
 
   server.listen({ port: 8080 }, (err, address) => {
     if (err) {
