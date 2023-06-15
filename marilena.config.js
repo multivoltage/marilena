a389f90b7f8de232cad9d47871164e2c9c7d6738
diego tonini <diego.tonini93@gmail.com>
2023-06-15 10:09:46 +0200

    fix: ignore input folder for development

--- marilena.config.js
+++ marilena.config.js
@@ -1,14 +1,19 @@
 /**
  * this is an example for a config file. Project use this under development
  */
+var path = require("path");
+
 module.exports = {
   inputFolder: "input",
   outputFolder: "output",
-  locales: ["en"],
+  locales: ["it"],
   templateOptions: {
     engine: "eta",
     variablesType: "yml",
     prepareEngine: (eta) => {
+      eta.configure({
+        views: path.join(process.cwd(), "input"),
+      });
       eta.templates.define("mypartial", eta.compile("PARTIAL SPEAKING"));
     },
   },
