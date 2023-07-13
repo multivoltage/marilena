d3589704ea1a3882cffdee379f01144aa738fb7f
diego tonini <diego.tonini93@gmail.com>
2023-07-13 09:55:32 +0200

    fix: throw error when use unsupported engine

--- src/types.ts
+++ src/types.ts
@@ -4,10 +4,12 @@ export interface Config {
   outputFolder: string; // default output
   textVersion?: (emailName: string, locale: string) => string;
   templateOptions?: {
-    engine: "eta" | "handlebars";
+    engine: SupportedEngine;
     // engineRoot will be handlebars,etc etc...
     prepareEngine: (engineRoot: unknown) => void;
   };
   locales: string[]; // default ["en"]
   mjmlParsingOptions?: MJMLParsingOptions;
 }
+
+export type SupportedEngine = "eta" | "handlebars";
