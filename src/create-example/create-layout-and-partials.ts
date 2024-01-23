1302cb837a998a1503630d33c16fd55bba6aa05b
diego tonini <diego.tonini93@gmail.com>
2024-01-23 22:56:55 +0100

    fix: update create example + add .nvmrc

--- src/create-example/create-layout-and-partials.ts
+++ src/create-example/create-layout-and-partials.ts
@@ -7,7 +7,7 @@ const layoutMain = `
         <mj-preview>hi user</mj-preview>
     </mj-head>
     <mj-include path="example/input/styles.css" type="css" css-inline="inline" />
-    <mj-body width="600px" background-color="#f4f4f4" class="gradient">
+    <mj-body width="600px" background-color="#f4f4f4">
 
       <!-- header -->
 
